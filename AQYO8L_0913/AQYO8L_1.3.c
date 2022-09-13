3b07728b4e9de9ff17eb8327931712427a4a677e
Stremler László <stremler.lacika77@gmail.com>
2022-09-13 11:00:39 +0200

    Harmadik feladat

---
+++ AQYO8L_0913/AQYO8L_1.3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+void pelda();
+
+int main()
+{
+    pelda();
+    return 0;
+}
+
+void pelda() {
+    FILE *fp1, *fp2;
+    char ch;
+    char fnev[50];
+    printf("Masolando fajlneve (50 karakter): ");
+    scanf("%s", fnev);
+    fp1 = fopen(fnev, "r");
+
+    if(!fp1) {
+        printf("Error.");
+        exit(0);
+    }
+
+    printf("Beilleszto fajlneve (50 karakter): ");
+    scanf("%s", fnev);
+    fp2 = fopen(fnev, "w");
+    while ((ch=getc(fp1)) != EOF) {
+        fputc(ch, fp2);
+    }
+    printf("A masolas elkeszult.");
+    fclose(fp1);
+    fclose(fp2);
+}
