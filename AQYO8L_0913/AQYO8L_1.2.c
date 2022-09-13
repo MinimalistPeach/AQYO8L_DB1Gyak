e9dd3a7f3de634cb31e0874d192d2e1ed34a1c4f
sm730bf <mesziist@gmail.com>
2022-09-13 10:50:57 +0200

    Masodik feladat

---
+++ AQYO8L_0913/AQYO8L_1.2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+void pelda();
+
+int main()
+{
+    pelda();
+    return 0;
+}
+
+void pelda() {
+    FILE *fp;
+    char ch;
+    char fnev[50];
+    printf("Fajlneve (MAX 50 karakter): ");
+    scanf("%s", fnev);
+    fp = fopen(fnev, "w");
+    printf("Fajlba irt szoveg: ");
+    while((ch=getchar()) != '#')
+    {
+        putc(ch, fp);
+    }
+    fclose(fp);
+
+    fp = fopen(fnev, "r");
+    while ((ch=getc(fp)) != EOF) {
+        printf("%c",toupper(ch));
+    }
+    fclose(fp);
+}
