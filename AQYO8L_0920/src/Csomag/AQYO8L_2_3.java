0bb53cbaaa4c5720c28a29b1c6c4794fd4d553a0
MinimalistPeach <stremler.lacika77@gmail.com>
2022-09-23 11:20:37 +0200

    2.gyak

---
+++ AQYO8L_0920/src/Csomag/AQYO8L_2_3.java
@@ -0,0 +1,33 @@
+package AQYO8L_0920.src.Csomag;
+import java.io.*;
+import java.util.Scanner;
+
+public class AQYO8L_2_3 {
+
+    public static void main(String[] args) throws IOException
+    {
+        beker();
+
+    }
+
+    public static void beker() throws IOException
+    {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Kérem a fájl nevét: ");
+        String fileName = reader.nextLine();
+
+        FileWriter fileWriter = new FileWriter(fileName);
+
+        do
+        {
+            System.out.println("Kérem a következő sort: ");
+            String temp = reader.nextLine();
+            fileWriter.write(temp+"\n");
+        }
+        while(!reader.nextLine().equals("end"));
+        
+        reader.close();
+        fileWriter.close();
+
+    }
+}
---
+++ AQYO8L_0920/src/Csomag/AQYO8L_2_4.java
@@ -0,0 +1,34 @@
+package AQYO8L_0920.src.Csomag;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AQYO8L_2_4 {
+
+    public static void main(String[] args) throws IOException
+    {
+        beOlvas();
+    }
+
+    public static void beOlvas() throws IOException
+    {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Kérem a fájl nevét: ");
+        String fileName = reader.nextLine();
+
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+        String line;
+        while ((line = br.readLine()) != null) 
+        {
+            System.out.println(line.toUpperCase());
+        }
+
+        reader.close();
+        br.close();
+
+    }
+    
+}
